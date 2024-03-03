7711923081f053678e3272d2d3577f6d75a10eea
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2024-03-03 10:30:01 -0500

    creacion DTO libro

---
+++ LibreriaORM/Data/LibroDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaORM.Data
+{
+    public class LibroDTO
+    {
+        public string Autor { get; set; }
+        public string Titulo { get; set; }
+        public int Anio { get; set; }
+        public Boolean Status { get; set; }
+        public string EditorialLibro { get; set; }
+    }
+}
