ee9d9db8f14e26c7f3f4f9fe9a7292cb9bcd3d87
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2024-03-03 11:04:19 -0500

    migraciones de prestamos

---
+++ LibreriaORM/Migrations/20240303155207_AddPrestamo.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibreriaORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrestamo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prestamo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fechaSalida = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fechaRegreso = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdPersona = table.Column<int>(type: "int", nullable: false),
+                    IdMaterialBibliografico = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamo", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Prestamo_MaterialBibliografico_IdMaterialBibliografico",
+                        column: x => x.IdMaterialBibliografico,
+                        principalTable: "MaterialBibliografico",
+                        principalColumn: "IdMaterialBibliografico",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Prestamo_Persona_IdPersona",
+                        column: x => x.IdPersona,
+                        principalTable: "Persona",
+                        principalColumn: "IdPersona",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestamo_IdMaterialBibliografico",
+                table: "Prestamo",
+                column: "IdMaterialBibliografico",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestamo_IdPersona",
+                table: "Prestamo",
+                column: "IdPersona",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamo");
+        }
+    }
+}
