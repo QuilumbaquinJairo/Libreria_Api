17866d0683ea4ef33fc11395fc9f3fce3daf59bb
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2024-03-03 01:38:05 -0500

    se agrega el contexto a nuestra API

--- LibreriaAPI/Program.cs
+++ LibreriaAPI/Program.cs
@@ -1,3 +1,9 @@
+using LibreriaORM;
+using LibreriaORM.Data;
+using LibreriaORM.Modelo;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +12,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LibreriaContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibreriaConecction"));
 
+});
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<LibreriaContext>();
+    dataContext.Database.Migrate();
+}
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
