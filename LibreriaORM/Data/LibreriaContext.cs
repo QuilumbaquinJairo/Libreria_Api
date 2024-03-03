b40c162b76fbc2b2ebd54c6365ec3d5a49184d16
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2024-03-03 10:29:42 -0500

    Mapeo de las claves en el contexto de la BD

--- LibreriaORM/Data/LibreriaContext.cs
+++ LibreriaORM/Data/LibreriaContext.cs
@@ -16,14 +16,32 @@ namespace LibreriaORM.Data
         public DbSet<Persona> Persona { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Administrador> Administrador { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Tesis> Tesis { get; set; }
+        public DbSet<Revista> Revista { get; set; }
+        public DbSet<MaterialBibliografico> MaterialBibliograficos { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Persona>().Property(p => p.Rol)
                 .HasConversion<string>();
+            modelBuilder.Entity<MaterialBibliografico>().Property(p => p.tipoMaterial)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Libro>().HasOne(l => l.MaterialBibliografico).WithOne()
+           .HasForeignKey<Libro>(l => l.IdMaterialBibliografico);
+            modelBuilder.Entity<Revista>().HasOne(l => l.MaterialBibliografico).WithOne()
+           .HasForeignKey<Revista>(l => l.IdMaterialBibliografico);
+            modelBuilder.Entity<Tesis>().HasOne(l => l.MaterialBibliografico).WithOne()
+           .HasForeignKey<Tesis>(l => l.IdMaterialBibliografico);
 
             modelBuilder.Entity<Persona>().ToTable("Persona");
             modelBuilder.Entity<Usuario>().ToTable("Usuario");
             modelBuilder.Entity<Administrador>().ToTable("Administrador");
+            modelBuilder.Entity<Libro>().ToTable("Libro");
+            modelBuilder.Entity<Tesis>().ToTable("Tesis");
+            modelBuilder.Entity<Revista>().ToTable("Revista");
+            modelBuilder.Entity<MaterialBibliografico>().ToTable("MaterialBibliografico");
+            base.OnModelCreating(modelBuilder);
 
         }
 
